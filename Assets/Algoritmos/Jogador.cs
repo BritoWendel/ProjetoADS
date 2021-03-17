853157868257c3dda724196807dda488927d9787
wendellbrito <wendelrfbrito@gmail.com>
2021-03-17 17:05:59 -0300

    criada cena teste, jogador com scripts e animacoes

---
+++ Assets/Algoritmos/Jogador.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    //variaveis publicas
+    public Rigidbody2D Rigidbody2D;
+    public float VelocidadeDeMovimento;
+    public SpriteRenderer SpriteRenderer;
+    public float ForcaDoPulo;
+    public Animator Animator;
+    //Variaveis privadas
+    private bool podePular;
+    EstadosDoJogador Estado;
+    void Start()
+    {
+        Estado = EstadosDoJogador.Ocioso;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        
+    }
+    private void FixedUpdate()
+    {
+        Andar();
+        Pular();
+    }
+    void Andar()
+    {
+        float eixohorizintal = Input.GetAxis("Horizontal");
+        Flipar(eixohorizintal);
+        eixohorizintal *= VelocidadeDeMovimento * Time.deltaTime;
+        Vector2 forca = new Vector2(eixohorizintal, 0);
+        Rigidbody2D.AddForce(forca, ForceMode2D.Force);
+    }
+    void Flipar(float valordoeixox)
+    {
+        if (valordoeixox > 0)
+            SpriteRenderer.flipX = false;
+        else if (valordoeixox < 0)
+            SpriteRenderer.flipX = true;
+    }
+    void Pular()
+    {
+        if (podePular)
+        {
+            float eixodopulo = Input.GetAxis("Jump");
+            eixodopulo *= ForcaDoPulo * Time.fixedDeltaTime;
+            Vector2 forca = new Vector2(0, eixodopulo); 
+            Rigidbody2D.AddForce(forca, ForceMode2D.Impulse);
+        }
+    }
+    void Animar()
+    {
+       
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Chao"))
+            podePular = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Chao"))
+            podePular = false;
+    }
+}
+enum EstadosDoJogador
+{
+    Ocioso = 1,
+    Andando = 2
+}
