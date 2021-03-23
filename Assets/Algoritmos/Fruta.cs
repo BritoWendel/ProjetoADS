91322666c8e87c01279cdd52befe2849a04aea22
wendellbrito <wendelrfbrito@gmail.com>
2021-03-23 10:40:06 -0300

    criado garra frutas e seus scripts

---
+++ Assets/Algoritmos/Fruta.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruta : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Destroy(this.gameObject);
+    }
+}
