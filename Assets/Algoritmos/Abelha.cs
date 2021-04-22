92483ac056e2fd5873a6717d94ac582d0448b16c
wendellbrito <wendelrfbrito@gmail.com>
2021-04-22 18:06:41 -0300

    terminado abelha e outros ajustes

--- Assets/Algoritmos/Abelha.cs
+++ Assets/Algoritmos/Abelha.cs
@@ -1,24 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Abelha : MonoBehaviour
 {
-    bool morreu;
-    CapsuleCollider2D cabeça;
-    // Start is called before the first frame update
-    void Start()
-    {
-        cabeça = GetComponent<CapsuleCollider2D>();
-    }
+    public GameObject OrigemDaBala, Bala;
+    public float IntervaloDeDisparo;
+    public float timer1, timer2;
+    public Area Area;
+    public Animator Animator;
+    public bool Morrendo;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Disparar();
+        Morrer();
+    }
+    void Disparar()
+    {
+        if (Area.EstaEmContato)
+        {
+            timer1 += Time.deltaTime;
+            if(timer1 > IntervaloDeDisparo)
+            {
+                this.Animator.SetTrigger("Atacando");
+                Instantiate(Bala, OrigemDaBala.transform);
+                timer1 = 0;
+            }
+        }
     }
     void Morrer()
     {
-        
+        if (Morrendo)
+        {
+            Animator.SetBool("Morrendo", true);
+            transform.Translate(0, -0.01f, 0);
+            timer2 += Time.deltaTime;
+            if(timer2 > 1)
+                Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Morrendo = true;
     }
 }
--- Assets/Algoritmos/Bala.cs
+++ Assets/Algoritmos/Bala.cs
@@ -1,17 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Bala : MonoBehaviour
 {
     public float Velocidade;
     public Vector3 Direcao;
+    float timer;
     private void Update()
     {
         Mover();
     }
     private void Mover()
     {
-        transform.position += Direcao * Velocidade * Time.deltaTime;
+        timer += Time.deltaTime;
+        if(timer >0.4)
+            transform.position += Direcao * Velocidade * Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
     }
 }
