34262b7a3e5695f4e2421381d0922eaec59a219f
wendellbrito <wendelrfbrito@gmail.com>
2021-05-15 14:10:14 -0300

    Atualização do menu e scripts

--- Assets/Algoritmos/Fase.cs
+++ Assets/Algoritmos/Fase.cs
@@ -6,13 +6,18 @@ public class Fase : MonoBehaviour
 {
     Jogador Jogador;
     AudioSource AudioSource;
+    Fim Fim;
+    private float contador;
+    private bool tocarUmaVez;
     public int NumeroDaFase;
     public AudioClip AudioClip;
+    public AudioClip MusicaFinal;
 
     private void Start()
     {
         Jogador = FindObjectOfType<Jogador>();
         AudioSource = GetComponent<AudioSource>();
+        Fim = FindObjectOfType<Fim>();
         AudioSource.clip = AudioClip;
         AudioSource.Play();
     }
@@ -25,16 +30,38 @@ public class Fase : MonoBehaviour
             Time.timeScale = 1;
         }
     }
+    private void Finalizar()
+    {
+        if (Fim.Ativado){
+            if (!tocarUmaVez)
+            {
+                AudioSource.Stop();
+                AudioSource.clip = MusicaFinal;
+                AudioSource.Play();
+                tocarUmaVez = true;
+            }
+            contador += Time.deltaTime;
+            if(contador > 3)
+                VoltarParaOMenu();
+        }
+    }
     private void Update()
     {
         Reiniciar();
+        Finalizar();
     }
     public void Pausar()
     {
         if (Time.timeScale == 1)
+        {
             Time.timeScale = 0;
+            AudioSource.Pause();
+        }
         else
+        {
             Time.timeScale = 1;
+            AudioSource.Play();
+        }
     }
     public void VoltarParaOMenu()
     {
--- Assets/Algoritmos/Fim.cs
+++ Assets/Algoritmos/Fim.cs
@@ -3,18 +3,23 @@
 public class Fim : MonoBehaviour
 {
     private Animator Animator;
-    private bool AtivarUmaVez;
+    private bool _ativarUmaVez;
+    public bool Ativado
+    {
+        get { return !_ativarUmaVez; }
+        private set { } 
+    }
     private void Start()
     {
-        AtivarUmaVez = true;
+        _ativarUmaVez = true;
         Animator = GetComponent<Animator>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && AtivarUmaVez)
+        if (collision.gameObject.CompareTag("Player") && _ativarUmaVez)
         {
             Animator.SetTrigger("Ativar");
-            AtivarUmaVez = false;
+            _ativarUmaVez = false;
         }
     }
 }
